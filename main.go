c84a8beef90a7b80fb711799765444f7d26fe017
Fahril Hadi <fahrilhadi77@gmail.com>
2025-02-04 00:06:14 +0700

    menambah dependency

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// nanti akan muncul contohnya
+go_say_hello "github.com/fahrilhadi/golang-modules"
+
+func main()  {
+	fmt.Println(go_say_hello.SayHello())
+}
+
+// gunakan go get github.com/fahrilhadi/golang-modules (alamat modules)
